da516391cf9654f78130a06dbccd38ca01b27ed9
Bernardo <bernardo181105@gmail.com>
2025-10-03 02:19:03 -0300

    feat: first vue components - Header title and Input of the todo

---
+++ vue_firebase_todo/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <h1 class="has-text-centered title">
+  Gerenciador de tarefas
+  </h1>
+
+</template>
---
+++ vue_firebase_todo/src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input v-model="NewTodoContent"
+               class="input"
+               type="text" placeholder="Adicione uma atividade"
+        >
+      </p>
+      <p class="control">
+        <button
+            class="button is-info"
+        >
+          Adicionar
+        </button>
+      </p>
+    </div>
+  </form>
+</template>
