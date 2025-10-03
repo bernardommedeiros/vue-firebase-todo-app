4edd53bd1d8d394908f56ca19f832edc8b236c85
Bernardo <bernardo181105@gmail.com>
2025-10-03 02:17:51 -0300

    feat: vue project initialization

---
+++ vue_firebase_todo/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Header from './components/Header.vue'
+import Input from "./components/Input.vue";
+</script>
+
+<template>
+  <div class="main">
+    <div  class="box mt-4">
+      <Header/>
+
+      <Input  />
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+.main {
+  max-width: 600px;
+  padding: 20px;
+  margin: 0 auto;
+}
+
+</style>
